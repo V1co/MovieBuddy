dfd615696ac83661c0ed5ca6d6e204d720ad7d51
V1co <rafal.pawlowski1993@yahoo.com>
2024-03-22 14:45:35 +0000

    feature: movies context and movie data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import './App.css'
 import Watchlist from './components/Watchlist'
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { MoviesContext } from './context/MoviesContext'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/watchlist">Watchlist</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/watchlist" element={<Watchlist />} />
-      </Routes>
-
+      <MoviesContext.Provider value= {{ movies, setMovies }}>
+        <header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/watchlist">Watchlist</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </MoviesContext.Provider>
     </BrowserRouter>
   )
 }
